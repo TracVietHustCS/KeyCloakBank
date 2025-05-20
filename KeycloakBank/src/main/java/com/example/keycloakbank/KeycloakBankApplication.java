c370a380f5d87a8ae89c426e6615eaf65ddccb04
mottocaat <155896816+mottocaat@users.noreply.github.com>
2025-05-20 23:23:20 +0700

    first commit

---
+++ KeycloakBank/src/main/java/com/example/keycloakbank/KeycloakBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloakbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakBankApplication.class, args);
+    }
+
+}
---
+++ KeycloakBank/src/test/java/com/example/keycloakbank/KeycloakBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloakbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
